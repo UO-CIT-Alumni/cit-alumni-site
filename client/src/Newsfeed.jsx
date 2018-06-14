610a0075dcf30b4c5924b02911ecbb9b1ab0b17a
Kory Schneider <kory.schneider@gmail.com>
2018-06-14 16:28:38 -0700

    Send API request

--- client/src/Newsfeed.jsx
+++ client/src/Newsfeed.jsx
@@ -5,6 +5,19 @@ export default class Newsfeed extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.getPosts()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  async getPosts() {
+    const response = await fetch('/api/posts');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  }
+
   render() {
     return (
       <h1>Newsfeed</h1>
